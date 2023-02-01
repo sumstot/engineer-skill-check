10008b9e3e21665c918d599c09525fa845661491
Soren Umstot <soren.umstot@gmail.com>
2023-02-01 17:34:42 +0900

    Notification paramsを追加して、Notificationsのviewsも書きました。

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,4 +8,15 @@ class NotificationsController < ApplicationController
     @employee = Employee.find_by(paramas[:id])
     @notification = Notification.find_by(params[:id])
   end
+
+  def new
+    @notification = Notification.new
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:title, :body)
+  end
+
 end
